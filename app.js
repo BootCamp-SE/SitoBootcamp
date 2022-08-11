e463f44d595fe2a9f2c4cd526226c2069cee8076
pCipriano98 <cipriano@teknautomazione.com>
2022-08-11 11:16:39 +0200

    [FIX] Static folder in different path

--- app.js
+++ app.js
@@ -4,13 +4,16 @@ const NewEntriesRoutes = require('./routes/newEntriesRoutes');
 const NewsRoutes = require('./routes/newsRoutes');
 const AccademyRoutes = require('./routes/accademyRoutes');
 
+const path = require("path"); // Require local path
+
 const LogRequests = (req, res, next) => {
     console.log(req.method, req.url);
     next();
 };
 
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+
+
 
 app.listen(3000);
 
@@ -23,6 +26,12 @@ app.get('/', (req, res) => {
 app.use('/arruolati', NewEntriesRoutes);
 app.use('/news', NewsRoutes);
 app.use('/accademia', AccademyRoutes);
+
+// Generate absolute path to static file
+app.use('/', express.static(path.join(__dirname + '/public')));
+app.use('/arruolati', express.static(path.join(__dirname + '/public')));
+app.use('/news', express.static(path.join(__dirname + '/public')));
+app.use('/accademia', express.static(path.join(__dirname + '/public')));
 
 app.use((req, res) => {
     res.status(404).render('404', { title: '404' });
