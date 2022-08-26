9db4de7df0614d20f652717c9709357604617315
Metiu19 <metiu19programmer@gmail.com>
2022-08-26 15:37:26 +0200

    Code refactor added comments and .env file support

--- app.js
+++ app.js
@@ -1,28 +1,36 @@
+require('dotenv').config(); // Import and config of env vars using dotenv module
+
+// Importing modules
 const express = require('express');
+const path = require('path');
+
+// Global Constants
+const Port = process.env.PORT | 3000;
+
+// Express Init
 const app = express();
 const NewEntriesRoutes = require('./routes/newEntriesRoutes');
 const NewsRoutes = require('./routes/newsRoutes');
 const AccademyRoutes = require('./routes/accademyRoutes');
 
-const path = require("path"); // Require local path
-
+// Middleware
 const LogRequests = (req, res, next) => {
-app.use('/', express.static(path.join(__dirname + '/public')));
-app.use('/', express.static(path.join(__dirname + '/public')));
-    console.log(req.method, req.url);
-    next();
+	console.log(req.method, req.url);
+	next();
 };
 
 app.set('view engine', 'ejs');
-
-
-
-app.listen(3000);
-
 app.use(LogRequests);
+app.use('/', express.static(path.join(__dirname + '/public')));
 
+// Starting web server
+const server = app.listen(Port, () => {
+	console.log(`Server listening on port: ${Port}`);
+});
+
+// Routes handling
 app.get('/', (req, res) => {
-    res.render('index', {title: "Home"});
+	res.render('index', { title: 'Home' });
 });
 
 app.use('/arruolati', NewEntriesRoutes);
@@ -31,5 +39,19 @@ app.use('/accademia', AccademyRoutes);
 
 // Handling requests to not existing pages (aka error 404)
 app.use((req, res) => {
-    res.status(404).render('404', { title: '404' });
+	res.status(404).render('404', { title: '404' });
 });
+
+// Stopping web server gracefully
+function StopServer(signal) {
+	console.log(`Recived ${signal}\nStopping web server!`);
+	server.closeAllConnections();
+	server.close(() => {
+		console.log('Web server stopped!\nExiting process!');
+		process.exit(0);
+	});
+}
+
+process.on('SIGINT', StopServer);
+process.on('SIGTERM', StopServer);
+process.on('SIGQUIT', StopServer);
